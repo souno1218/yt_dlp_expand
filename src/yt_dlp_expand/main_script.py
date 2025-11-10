9be951eb18c4a104e68771f814e119eebcff592c
souno1218 <supinosaurusuz@gmail.com>
2025-11-10 16:45:21 +0900

    Add os_name "iOS"

--- src/yt_dlp_expand/main_script.py
+++ src/yt_dlp_expand/main_script.py
@@ -1,26 +1,114 @@
 ## https://qiita.com/soun1218/items/3f07fbaa7029208dd789
 
+from mutagen.flac import Picture
 from mutagen.id3 import APIC, ID3
 from mutagen.oggopus import OggOpus
-from mutagen.flac import Picture
-import os, ffmpeg, base64, pathlib, argparse, platform, subprocess, random, string
 from concurrent.futures import ThreadPoolExecutor
+import os, ffmpeg, base64, pathlib, argparse, platform, subprocess, random, string
+
+
+def main():
+    parser = argparse.ArgumentParser()
+    txt = "0:bestaudio(mp3),\n1:bestaudio(opus),\n2:720p,mp4(h264,mp4a),\n3:bestvideo(mp4(h264,mp4a)),\n4:bestvideo(mp4(vp9,opus))"
+    parser.add_argument("DownloadMode", type=int, help=txt, choices=list(range(5)))
+    parser.add_argument("url", type=str, help="url")
+    parser.add_argument("-p", "--path", type=str, help="downloadMode dir path")
+    parser.add_argument(
+        "-l", "--playlist", type=bool, help="use playlist or not ", default=False
+    )
+    args = parser.parse_args()
+    if args.playlist and ("&list=" in args.url):
+        is_pc = check_is_pc()
+        if args.path is None:
+            if is_pc:
+                dir_path = "~/Downloads"
+            else:
+                dir_path = "~/Documents"
+        else:
+            dir_path = args.path if args.path[-1] != "/" else args.path[:-1]
+
+        dir_path = pathlib.Path(dir_path).expanduser()
+
+        playlist_title = get_playlist_title(args.url)
+        if playlist_title is None:
+            print("Failed to retrieve playlist title.")
+        elif not os.path.isfile(dir_path):
+            dir_path = f"{dir_path}/{playlist_title}"
+
+        if os.path.isfile(dir_path):
+            raise ValueError(f"A path specified as a directory is a file : {dir_path}")
+        if not os.path.isdir(dir_path):
+            os.makedirs(dir_path)
+
+        list_url = split_playlist_url(args.url)
+        for i in list_url:
+            Class_Yt_dlp = ExpandYt_dlp(args.DownloadMode, i, dir_path)
+            Class_Yt_dlp.main_func()
+    else:
+        Class_Yt_dlp = ExpandYt_dlp(args.DownloadMode, args.url, args.path)
+        Class_Yt_dlp.main_func()
+
+
+def get_playlist_title(playlist_url):
+    script = (
+        f"yt-dlp '{playlist_url}' "
+        "-I 1:1 "
+        "--print '%(playlist_title)s' "
+        "--skip-download "
+        "--flat-playlist "
+        "--no-check-certificate "
+    )
+    cp = subprocess.run(
+        script,
+        encoding="utf-8",
+        stdout=subprocess.PIPE,
+        stderr=subprocess.PIPE,
+        shell=True,
+    )
+    playlist_title = cp.stdout.split("\n")[0]
+
+    replaceList = {":": "-", "[": "「", "]": "」", "/": "／", "\n": " ", "'": "’"}
+    for key, value in replaceList.items():
+        playlist_title = playlist_title.replace(key, value)
+
+    if len(playlist_title) == 0:
+        return None
+    else:
+        return playlist_title
+
+
+def split_playlist_url(playlist_url):
+    script = f"yt-dlp '{playlist_url}' --print '%(url)s' --skip-download --flat-playlist --no-check-certificate"
+    cp = subprocess.run(
+        script,
+        encoding="utf-8",
+        stdout=subprocess.PIPE,
+        stderr=subprocess.PIPE,
+        shell=True,
+    )
+    list_url = []
+    for i in cp.stdout.split("\n"):
+        if not len(i) == 0:
+            list_url.append(i)
+
+    if len(list_url) == 0:
+        raise ValueError("Failed to retrieve playlist url.")
+    else:
+        return list_url
+
 
 def check_is_pc():
     os_name = platform.system()
-    if not os_name in [
-        "Darwin",
-        "Linux",
-        "Windows",
-    ]:
-        raise ValueError("Use on an operating system that is not intended for")
-
-    is_pc = True
-    if os_name == "Darwin":
-        device = platform.platform().split("-")[2]
+    device = platform.platform()  # .split("-")[2]
+    if os_name in ["Darwin", "iOS"]:
         if ("iPhone" in device) or ("iPad" in device):
-            is_pc = False
-    return is_pc
+            return False
+        else:
+            return True
+    elif os_name in ["Linux", "Windows"]:
+        return True
+    else:
+        raise ValueError("Use on an operating system that is not intended for")
 
 
 def exist(path):
@@ -28,7 +116,7 @@ def exist(path):
 
 
 def randomstr(n):
-    return ''.join(random.choices(string.ascii_letters + string.digits, k=n))
+    return "".join(random.choices(string.ascii_letters + string.digits, k=n))
 
 
 class ExpandYt_dlp:
@@ -48,7 +136,9 @@ class ExpandYt_dlp:
                 path = "~/Documents"
         self.output_path = pathlib.Path(path).expanduser()
         if os.path.isfile(self.output_path):
-            raise ValueError(f"A path specified as a directory is a file : {self.output_path}")
+            raise ValueError(
+                f"A path specified as a directory is a file : {self.output_path}"
+            )
         if not os.path.isdir(self.output_path):
             os.makedirs(self.output_path)
 
@@ -84,7 +174,13 @@ class ExpandYt_dlp:
             "--no-check-certificate "
             "--no-playlist "
         )
-        cp = subprocess.run(script, encoding="utf-8", stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True)
+        cp = subprocess.run(
+            script,
+            encoding="utf-8",
+            stdout=subprocess.PIPE,
+            stderr=subprocess.PIPE,
+            shell=True,
+        )
         self.title = cp.stdout.replace("\n", "")
         replaceList = {":": "-", "[": "「", "]": "」", "/": "／", "\n": " ", "'": "’"}
         for key, value in replaceList.items():
@@ -109,7 +205,9 @@ class ExpandYt_dlp:
             "--convert-thumbnails jpg "
             f"--output '{thumbnail_path_no_ext}'"
         )
-        subprocess.run(script, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True)
+        subprocess.run(
+            script, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True
+        )
 
         if exist(f"{thumbnail_path_no_ext}.jpg"):
             print(f"download_thumbnail_jpg Done")
@@ -138,10 +236,14 @@ class ExpandYt_dlp:
                     '+bestaudio[acodec~=mp4a]" '
                 )
             case 3:
-                script += "-f \"bestvideo*[vcodec~='^(avc|h264)']+bestaudio[acodec~=mp4a]\" "
+                script += (
+                    "-f \"bestvideo*[vcodec~='^(avc|h264)']+bestaudio[acodec~=mp4a]\" "
+                )
             case 4:
                 script += "-f 'bestvideo+bestaudio/best' --merge-output-format mp4"
-        subprocess.run(script, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True)
+        subprocess.run(
+            script, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True
+        )
 
         if exist(f"{file_path_no_ext}.{self.ext}"):
             print("download_file Done")
@@ -152,7 +254,9 @@ class ExpandYt_dlp:
             raise ValueError("Failed download_file")
 
     def marge_file_thumbnail_mp4(self):
-        video = ffmpeg.input(f"{self.output_path}/{self.random_title}_before.{self.ext}")
+        video = ffmpeg.input(
+            f"{self.output_path}/{self.random_title}_before.{self.ext}"
+        )
         cover = ffmpeg.input(f"{self.output_path}/{self.random_title}.jpg")
         (
             ffmpeg.output(
@@ -246,8 +350,10 @@ class ExpandYt_dlp:
                 self.marge_file_thumbnail_mp4()
             case 4:  # bestvideo(mp4(vp9,opus))
                 self.marge_file_thumbnail_mp4()
-        os.rename(f"{self.output_path}/{self.random_title}.{self.ext}", 
-                  f"{self.output_path}/{self.title}.{self.ext}")
+        os.rename(
+            f"{self.output_path}/{self.random_title}.{self.ext}",
+            f"{self.output_path}/{self.title}.{self.ext}",
+        )
         if exist(f"{self.output_path}/{self.title}.{self.ext}"):
             print("all process Done")
         else:
